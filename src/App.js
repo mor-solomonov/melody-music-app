bbfb8df2ad7af5cae970db5042c6151a4a9f2403
Mor_aka_Gaia <mor.solomonov@gmail.com>
2020-11-11 13:04:05 +0200

    added state in App.js and used it via props in Song component. added styles for song.scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/app.scss';
 //Adding Components
 import Player from './components/Player';
@@ -6,9 +6,13 @@ import Song from './components/Song';
 import data from './util';
 
 function App() {
+    //State
+    const [songs, setSongs] = useState(data());
+    const [currentSong, setCurrentSong] = useState(songs[0]);
+
     return (
         <div className='App'>
-            <Song />
+            <Song currentSong={currentSong} />
             <Player />
 
         </div>
