db41701f9758619fd25e5ae4f3db201b5753afb9
Mor_aka_Gaia <mor.solomonov@gmail.com>
2020-11-11 13:23:07 +0200

    added an event handler for the player component and used useREf to access the audio file in the data

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,31 +1,43 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-    faPlay,
-    faAngleLeft,
-    faAngleRight,
-} from "@fortawesome/free-solid-svg-icons";
+  faPlay,
+  faAngleLeft,
+  faAngleRight
+} from '@fortawesome/free-solid-svg-icons';
 
+const Player = ({ currentSong }) => {
+  //Ref
+  const audioRef = useRef(null);
+  //Event Handlers
+  const playSongHandler = () => {
+    console.log(audioRef.current);
+  };
 
-const Player = () => {
-    return (
-        <div className="player">
-            <div className="time-control">
-                <p>Start Time</p>
-                <input type="range" />
-                <p>End Time</p>
-            </div>
-            <div className="play-control">
-                <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
-                <FontAwesomeIcon
-                    className="skip-forward"
-                    size="2x"
-                    icon={faAngleRight}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className='player'>
+      <div className='time-control'>
+        <p>Start Time</p>
+        <input type='range' />
+        <p>End Time</p>
+      </div>
+      <div className='play-control'>
+        <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleLeft} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className='play'
+          size='2x'
+          icon={faPlay}
+        />
+        <FontAwesomeIcon
+          className='skip-forward'
+          size='2x'
+          icon={faAngleRight}
+        />
+      </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
+    </div>
+  );
 };
 
 export default Player;
